d427c9f872edb23eb7d86851c18bc3519fd4d858
unknown <techtijmen@gmail.com>
2018-05-17 22:19:11 +0200

    Testing websocket

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var http = require('http');
 var url = require('url');
+var io = require('socket.io', {
+    transports: ['websocket']
+})(http);
 
 var server = http.createServer(function(req, res) { //creates node server
     res.writeHead(200, {"Content-Type": "text/html"});
@@ -7,6 +10,10 @@ var server = http.createServer(function(req, res) { //creates node server
     console.log(visitor);
     res.write('Welcome to da page!');
     res.end();
+});
+
+io.on('connection', function (socket) {
+    console.log('Client connected!');
 });
 
 server.listen(8080);
