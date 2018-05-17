186945a924ebb5c2a4535048b4f07041ee3e8c45
techtijmen <39381506+techtijmen@users.noreply.github.com>
2018-05-17 19:25:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var http = require('http');
 var url = require('url');
-var EventEmitter = require('events').EventEmitter;
 
 var server = http.createServer(function(req, res) { //creates node server
     res.writeHead(200, {"Content-Type": "text/html"});
@@ -9,10 +8,4 @@ var server = http.createServer(function(req, res) { //creates node server
     res.end();
 });
 
-server.on('close', function() { // We listened to the close event
-    console.log('Goodbye!');
-});
-
 server.listen(8080);
-
-server.close();
