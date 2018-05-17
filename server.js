60b43f8ee76338646b64d89170d8c1a8205e3bd9
unknown <techtijmen@gmail.com>
2018-05-17 20:11:19 +0200

    More Testing

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var url = require('url');
 var server = http.createServer(function(req, res) { //creates node server
     res.writeHead(200, {"Content-Type": "text/html"});
     var visitor = url.parse(req.url).pathname;
+    console.log(visitor);
     res.write('Welcome to da page!');
     res.end();
 });
