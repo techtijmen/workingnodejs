ef14e0bbcc1d02d3018fb668ca7917d8b0062198
techtijmen <39381506+techtijmen@users.noreply.github.com>
2018-05-18 18:18:16 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 var Config = {
   // Contents of this file will be send to the client
-  "domain":     process.env.OPENSHIFT_APP_DNS || '127.0.0.1',
+  "domain":     process.env.OPENSHIFT_APP_DNS || 'http://workingnodejs-workingnodejs.193b.starter-ca-central-1.openshiftapps.com/',
 
-  "serverip":   process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
+  "serverip":   process.env.OPENSHIFT_NODEJS_IP || 'http://workingnodejs-workingnodejs.193b.starter-ca-central-1.openshiftapps.com/',
   "serverport": process.env.OPENSHIFT_NODEJS_PORT || '8080',
   
   "clientport": (process.env.OPENSHIFT_NODEJS_PORT) ? '8000':'8080',
