02ea8c5a29140a231893cb16574fcf3348cc1f54
techtijmen <39381506+techtijmen@users.noreply.github.com>
2018-05-19 15:54:43 +0200

    Update server.js

--- server.js
+++ server.js
@@ -27,6 +27,6 @@ io.on('connection', function(socket){
   console.log('a user connected');
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(8080, function(){
+  console.log('listening on *:8080');
 });
